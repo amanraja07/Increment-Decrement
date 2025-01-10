f9d95c9bbd69db97e05ffaec62432bb053fbf108
Aman Raja <110588951+amanraja07@users.noreply.github.com>
2025-01-10 21:59:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const countValue = document.querySelector('#counter');
+
+const increment = ()=> {
+    //get value from UI.
+    let value = parseInt(countValue.innerText);
+    //Update value .
+    value += 1;
+    //SET VALUE TO THE UI.
+    countValue.innerText = value;
+}
+
+const decrement = ()=> {
+    //get value from UI.
+    let value = parseInt(countValue.innerText);
+    //Update value .
+    value -= 1;
+    //SET VALUE TO THE UI.
+    countValue.innerText = value;
+}
